8c60a371eed609fee180fc481fa7538f32a03e48
ecuadros <ecuadros@spc.org.pe>
2025-09-08 22:11:01 -0500

    Basic function ok

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "test.h"
 
 using namespace std;
 
@@ -7,8 +8,16 @@ using namespace std;
 // 3. Alex Segura
 // 4. Juan Felix Arias Ramos
 // 5. Oscar Toledo Guerrero
-// 
+
+// Forma 1 de Compilar: 
+// g++ -std=c++17 -Wall -g -pthread -o main main.cpp test.cpp
+// Forma #2 de Compilar (requiere el archivo Makefile)
+// make
+
 int main(){
     cout << "Hello EDA - MCC639G" <<endl;
+    int t = prueba(5);
+    cout << "Respuesta:" << t << endl;
+
     return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,6 @@
+
+#include "test.h"
+
+int prueba(int n){
+    return n*n;
+}
