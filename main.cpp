fce691c13dc9a72e66a4986d0431b841793f6731
unknown <luisantonioselisvasquez@gmail.com>
2025-09-08 22:42:49 -0500

    Agrego función suma y pruebo main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include "test.h"
 
 using namespace std;
 
+// 1. Ernesto Cuadros-Vargas
+// 2. Luis Antonio Selis Vasquez
 // 3. Alex Segura
 // 4. Juan Felix Arias Ramos
+// 5. Oscar Toledo Guerrero
+
+// Forma 1 de Compilar: 
+// g++ -std=c++17 -Wall -g -pthread -o main main.cpp test.cpp
+// Forma #2 de Compilar (requiere el archivo Makefile)
+// make
 
 int main(){
     cout << "Hello EDA - MCC639G" <<endl;
+    int t = prueba(5);
+    cout << "Respuesta:" << t << endl;
+
+    // NUEVO: usando la función suma, Luis Selis
+    int resultado = suma(3, 7);
+    cout << "Suma de 3 + 7 = " << resultado << endl;
+
     return 0;
 }
---
+++ test.h
@@ -0,0 +1,7 @@
+#ifndef __TEST_H__
+#define __TEST_H__
+
+int prueba(int n);
+int suma(int a, int b);     // nueva función Luis Selis
+
+#endif
