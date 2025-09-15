5e7a8649c817dcbfdf26aa2f38a240d6a9f8690d
ecuadros <ecuadros@spc.org.pe>
2025-09-15 18:53:09 -0500

    Lista completa de alumnos

--- main.cpp
+++ main.cpp
@@ -6,7 +6,11 @@ using namespace std;
 // 2. Luis Antonio Selis Vasquez
 // 3. Alex Segura
 // 4. Juan Felix Arias Ramos
-// 5.
+// 5. Oscar Toledo
+// 6. Richard Villanueva
+// 7. José Bazán
+// 8. Aldo Arriola
+// 9. David Quispe
 
 int main(){
     cout << "Hello EDA - MCC639G" <<endl;
