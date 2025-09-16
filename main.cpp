a8724c4eb1efe1e96d3877ffa531d10aa313317f
aldo <aldo.arriolac@gmail.com>
2025-09-15 19:08:47 -0500

    Aldo's name added

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 // 3. Alex Segura
 // 4. Juan Felix Arias Ramos
 // 5. Oscar Toledo Guerrero
-
+// 6. Aldo Arriola
 // Forma 1 de Compilar: 
 // g++ -std=c++17 -Wall -g -pthread -o main main.cpp test.cpp
 // Forma #2 de Compilar (requiere el archivo Makefile)
