528b5f5bd387c776649269be7b97130b187bc48f
Oscar Toledo <tgoscar@gmail.com>
2025-12-01 20:15:22 -0500

    AVL v1 OTG
    Added balance factor management and rotation methods for AVL tree.

--- avl.h
+++ avl.h
@@ -11,6 +11,10 @@ public:
 protected:
     int     m_balanceFactor = 0; // Balance factor for AVL tree
 public:
+    CAVLNode() : Base(), m_balanceFactor(0) {}
+    
+    int getBalanceFactor() const { return m_balanceFactor; }
+    void setBalanceFactor(int bf) { m_balanceFactor = bf; }
 };
 
 template <typename _T>
@@ -23,8 +27,8 @@ struct AVLAscTraits{
 template <typename _T>
 struct AVLDescTraits{
     using  value_type = _T;
-    using  Node       = CAVLNode<T>;
-    using  CompareFn  = greater<T>;
+    using  Node       = CAVLNode<_T>;
+    using  CompareFn  = greater<_T>;
 };
 
 template <typename Traits>
@@ -39,17 +43,120 @@ public:
 
 protected:
     // Additional members for AVL tree balancing can be added here
+    using AVLNode = CAVLNode<Traits>;
+    // ========================================================================
+    // Funciones auxiliares
+    // ========================================================================
+    
+    int getHeight(Node* node) {
+        if (!node) return 0;
+        return 1 + std::max(getHeight(node->m_pLeft), getHeight(node->m_pRight));
+    }
+    
+    int calculateBalance(Node* node) {
+        if (!node) return 0;
+        return getHeight(node->m_pLeft) - getHeight(node->m_pRight);
+    }
+    
+    void updateBalanceFactor(Node* node) {
+        if (node) {
+            int bf = calculateBalance(node);
+            static_cast<AVLNode*>(node)->setBalanceFactor(bf);
+        }
+    }
+    
+    // ========================================================================
+    // Rotaciones
+    // ========================================================================
+    
+    Node* rotateRight(Node* y) {
+        Node* x = y->m_pLeft;
+        Node* T2 = x->m_pRight;
+        
+        x->m_pRight = y;
+        y->m_pLeft = T2;
+        
+        x->m_pParent = y->m_pParent;
+        y->m_pParent = x;
+        if (T2) T2->m_pParent = y;
+        
+        updateBalanceFactor(y);
+        updateBalanceFactor(x);
+        
+        return x;
+    }
+    
+    Node* rotateLeft(Node* x) {
+        Node* y = x->m_pRight;
+        Node* T2 = y->m_pLeft;
+        
+        y->m_pLeft = x;
+        x->m_pRight = T2;
+        
+        y->m_pParent = x->m_pParent;
+        x->m_pParent = y;
+        if (T2) T2->m_pParent = x;
+        
+        updateBalanceFactor(x);
+        updateBalanceFactor(y);
+        
+        return y;
+    }
+    
+    // ========================================================================
+    // Balanceo
+    // ========================================================================
+    
+    Node* balance(Node* node) {
+        if (!node) return node;
+        
+        updateBalanceFactor(node);
+        int bf = static_cast<AVLNode*>(node)->getBalanceFactor();
+        
+        // Izquierda-Izquierda
+        if (bf > 1 && calculateBalance(node->m_pLeft) >= 0) {
+            return rotateRight(node);
+        }
+        
+        // Derecha-Derecha
+        if (bf < -1 && calculateBalance(node->m_pRight) <= 0) {
+            return rotateLeft(node);
+        }
+        
+        // Izquierda-Derecha
+        if (bf > 1 && calculateBalance(node->m_pLeft) < 0) {
+            node->m_pLeft = rotateLeft(node->m_pLeft);
+            return rotateRight(node);
+        }
+        
+        // Derecha-Izquierda
+        if (bf < -1 && calculateBalance(node->m_pRight) > 0) {
+            node->m_pRight = rotateRight(node->m_pRight);
+            return rotateLeft(node);
+        }
+        
+        return node;
+    }
     // TODO: modificar la insercion para que mantenga
     //       el balance del arbo y realice las 
     //       rotaciones necesarias
-    Node *internal_insert(value_type &elem, Ref ref,
+    virtual Node *internal_insert(value_type &elem, Ref ref,
                           Node* pParent, Node*& rpOrigin) override
     {
         // TODO 1. insertar
-        // TODO 2. verificar balance
-        // TODO 3. realizar rotaciones si es necesario
+        Node* inserted = internal_insert(elem, ref, pParent, rpOrigin);
         
-        return nullptr;
+        // TODO 2. verificar balance
+        if (inserted) {
+            // TODO 2. verificar balance
+            updateBalanceFactor(rpOrigin);
+            
+            // TODO 3. realizar rotaciones si es necesario
+            rpOrigin = balance(rpOrigin);
+        }
+        
+      
+        return inserted;
     }
 public:
     CAVLTree() : Base() {} // Empty tree
